1206585f2c62f6939db61dd88ad4abca1f70173d
bwillows <152449409+bwillows@users.noreply.github.com>
2025-06-07 21:04:55 -0700

    Update PlayerHealth.java

--- main/java/bwillows/playerhealth/PlayerHealth.java
+++ main/java/bwillows/playerhealth/PlayerHealth.java
@@ -13,54 +13,68 @@ import org.bukkit.scoreboard.*;
 
 public final class PlayerHealth extends JavaPlugin implements Listener {
 
-    Plugin plugin;
-    ScoreboardManager scoreboardManager;
+    private Plugin plugin;
+    private ScoreboardManager scoreboardManager;
+    private Scoreboard globalBoard; // Global scoreboard for all players
 
     @Override
     public void onEnable() {
         plugin = this;
         scoreboardManager = Bukkit.getScoreboardManager();
+        // Create a global scoreboard to be used for all players
+        globalBoard = scoreboardManager.getNewScoreboard();
         // Plugin startup logic
         Bukkit.getPluginManager().registerEvents(this, plugin);
+
+        Bukkit.getScheduler().runTaskTimer(this, this::updateAllPlayersHealth, 0L, 20L);
     }
 
+    // Method to update health for a specific player
     public void updateHealth(Player player) {
         Scoreboard board = player.getScoreboard();
 
-        Objective healthDisplay = board.getObjective("healthDisplay");
-        if(healthDisplay == null) {
-            healthDisplay = board.registerNewObjective("healthDisplay", "placeholder");
+        // Always ensure the player has the global scoreboard
+        if (board != globalBoard) {
+            player.setScoreboard(globalBoard);
+        }
+
+        Objective healthDisplay = globalBoard.getObjective("healthDisplay");
+        if (healthDisplay == null) {
+            healthDisplay = globalBoard.registerNewObjective("healthDisplay", "health");
             healthDisplay.setDisplaySlot(DisplaySlot.BELOW_NAME);
             healthDisplay.setDisplayName(ChatColor.RED + "❤");
         }
+        // Make sure the score is updated for the player
         Score score = healthDisplay.getScore(player.getName());
         score.setScore((int) player.getHealth());
     }
 
-    public void startUpdatingScoreboard(Player player) {
-        Bukkit.getScheduler().runTaskTimer(this, () -> {
+    // Method to update health for all players
+    private void updateAllPlayersHealth() {
+        for (Player player : Bukkit.getOnlinePlayers()) {
             updateHealth(player);
-        }, 0L, 5L); // 20L = 1 second
+        }
     }
 
+    // Create a scoreboard for a player (but we always set them to the global scoreboard)
     public void createScoreboard(Player player) {
-        Scoreboard board = scoreboardManager.getNewScoreboard();
-        player.setScoreboard(board);
+        player.setScoreboard(globalBoard); // Set all players to use the global scoreboard
     }
 
     @EventHandler
     public void playerJoinEvent(PlayerJoinEvent event) {
         Player player = event.getPlayer();
         createScoreboard(player);
-        startUpdatingScoreboard(player);
+        // Update health immediately when the player joins
+        updateHealth(player);
     }
 
     @EventHandler
     public void entityDamageEvent(EntityDamageEvent event) {
-        if(!(event.getEntity() instanceof Player))
-            return;
-
-        updateHealth((Player) event.getEntity());
+        if (event.getEntity() instanceof Player) {
+            Player player = (Player) event.getEntity();
+            updateHealth(player);  // Update health when the player takes damage
+        }
     }
 
     @Override
